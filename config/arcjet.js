dda4e4580df0428c0dc687e678b362bc084d429b
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2025-09-13 04:43:17 +0100

    arjcet prod

--- config/arcjet.js
+++ config/arcjet.js
@@ -2,21 +2,21 @@ import arcjet, { shield, tokenBucket, detectBot } from '@arcjet/node';
 import { ARCJET_KEY } from './env.js';
 
 const aj = arcjet({
-  key: ARCJET_KEY,
-  characteristics: ['ip.src'],
-  rules: [
-    shield({ mode: 'DRY_RUN' }),
-    tokenBucket({
-      mode: 'LIVE',
-      refillRate: 5,  
-      interval: 10,   
-      capacity: 10,
-    }),
-    detectBot({
-      mode: 'DRY_RUN',
-      allow: ['CATEGORY:SEARCH_ENGINE'],
-    }),
-  ],
+	key: ARCJET_KEY,
+	characteristics: ['ip.src'],
+	rules: [
+		shield({ mode: 'LIVE' }),
+		tokenBucket({
+			mode: 'LIVE',
+			refillRate: 5,
+			interval: 10,
+			capacity: 10,
+		}),
+		detectBot({
+			mode: 'LIVE',
+			allow: ['CATEGORY:SEARCH_ENGINE'],
+		}),
+	],
 });
 
 export default aj;
